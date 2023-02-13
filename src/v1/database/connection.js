ca70edaa577de4977442b7ee2a5f40d9423e549c
OctoSheep_Manjaro <zyymaker@outlook.com>
2023-02-14 00:45:07 +0800

    连接 MongoDB 并创建单词和词汇书的 Schema

---
+++ src/v1/database/connection.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023. OctoSheep
+ *
+ * This file is part of Word Power.
+ *
+ * Word Power is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * Word Power is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Word Power. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+try {
+    mongoose.connect('mongodb://127.0.0.1:27017/power');
+} catch (e) {
+    console.log(e);
+}
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Connected to MongoDB');
+});
+
+module.exports = db;
